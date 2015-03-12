f7ef33e4ef595099467726e756abddfba0ce7b16
Felix Novovic <felix.novovic@senionlab.com>
2015-03-12 20:13:49 +0100

    More on the gui design. Implemented the profile template and view.

--- twidder/static/js/App.js
+++ twidder/static/js/App.js
@@ -1,11 +1,31 @@
 define([
 	'jquery',
+	'handlebars',
 	'Router'
-], function($, Router) {
+], function($, Handlebars, Router) {
 
 	var App = function() {  }
 
 	App.prototype.init = function() {
+		Handlebars.registerHelper('faGender', function(gender) {
+
+			var male = 'fa-mars';
+			var female = 'fa-venus';
+			var defaultGender = 'fa-transgender'; // Yeah. If you're neither you're both.
+
+			var genderMap = {
+				'male': 'fa-mars',
+				'female': 'fa-venus',
+				'transgender': 'fa-transgender'
+			};
+
+			var faUse = genderMap[gender.toLowerCase()];
+			if(typeof faUse === 'undefined') {
+				faUse = 'fa-mercury'; // I don't know?
+			}
+
+			return faUse;
+		});
 
 		/* Starting the application router. */
 		var appRouter = new Router();
--- twidder/static/js/views/ProfileView.js
+++ twidder/static/js/views/ProfileView.js
@@ -18,11 +18,31 @@ define([
 		},
 
 		render: function() {
-			var compiledTemplate = Handlebars.compile(ProfileViewTemplate);
+			var self = this;
 
-			var templateHtml = compiledTemplate();
+			var userDeferred = $.get('/api/user');
+			var postsDeferred = $.get('/api/posts')
 
-			this.$el.html(templateHtml);
+			$.when(userDeferred, postsDeferred).done(function(userResponse, postResponse) {
+				var userData = userResponse[0];
+				var postData = postResponse[0];
+
+				if(!userData.success) {
+					Backbone.history.navigate('/login', {trigger: true});
+					return;
+				}
+
+				var templateData = {
+					user: userData.data,
+					posts: postData
+				};
+
+				var compiledTemplate = Handlebars.compile(ProfileViewTemplate);
+
+				var templateHtml = compiledTemplate(templateData);
+
+				self.$el.html(templateHtml);
+			});
 		}
 
 	});
